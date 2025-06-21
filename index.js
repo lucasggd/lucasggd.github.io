e5a74af897e7fda7d3c5576681be0ee30f0cbcad
Lucas <lucas.dias@zion.local>
2025-06-21 16:46:28 -0300

    [FEAT] Services section + Contact section

--- index.js
+++ index.js
@@ -37,3 +37,23 @@ const observerCompaniesSection = new IntersectionObserver((entries) => {
 });
 
 observerCompaniesSection.observe(document.querySelector("#exp-title"));
+
+// Trigger tooling title animation
+const observerServicesSection = new IntersectionObserver((entries) => {
+  // Loop over the entries
+  entries.forEach((entry) => {
+    const titleEl = document.querySelector("#service-title");
+    const itemsEl = document.querySelector("#services-list");
+
+    if (entry.isIntersecting) {
+      titleEl.classList.add("tooling-title-animation");
+      itemsEl.classList.add("tooling-items-div-animation");
+      return;
+    }
+
+    titleEl.classList.remove("tooling-title-animation");
+    itemsEl.classList.remove("tooling-items-div-animation");
+  });
+});
+
+observerServicesSection.observe(document.querySelector("#service-title"));
